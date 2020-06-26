b6f0e1408fa91da553e9f42b7c3440d2a2c80cf7
Daniel L Correia <dlcorreia.ssa@gmail.com>
2020-06-25 22:30:03 -0300

    organizou o código

---
+++ scripts/cenas/jogo.js
@@ -0,0 +1,89 @@
+class Jogo {
+    constructor () {
+        this.inimigoAtual = 0;
+    }
+
+    setup () {
+        cenario = new Cenario(imagemPlanoDeFundo, 3);
+  pontuacao = new Pontuacao();
+  personagem = new Personagem(matrizHipsta, 
+                        imagemHipsta,
+                        0,
+                        110,
+                        135,
+                        30,
+                        220,
+                        270);
+  const inimigo = new Inimigo(matrizInimigo, 
+                        imagemInimigo,
+                        width -52,
+                        52,
+                        52,
+                        30,
+                        104,
+                        104,
+                        10,
+                        100);
+  const inimigoGrande = new Inimigo(matrizInimigoGrande, 
+                        imagemInimigoGrande,
+                        width -52,
+                        200,
+                        200,
+                        0,
+                        400,
+                        400,
+                        10,
+                        100);  
+  const inimigoVoador = new Inimigo(matrizInimigoVoador, 
+                        imagemInimigoVoador,
+                        width -52,
+                        52,
+                        52,
+                        200,
+                        200,
+                        150,
+                        5,
+                        100); 
+
+    inimigos.push(inimigo);
+    inimigos.push(inimigoGrande);
+    inimigos.push(inimigoVoador);
+    }
+
+    keyPressed(key) {
+        if(key === 'ArrowUp') {
+            personagem.pula();
+          }
+    }
+
+    draw() {
+        cenario.exibe();
+        cenario.move();
+      
+        pontuacao.exibe();
+        pontuacao.adicionarPonto();
+        
+        personagem.exibe();
+        personagem.aplicaGravidade();
+        
+        const inimigo = inimigos[this.inimigoAtual];
+        const inimigoVisivel = inimigo.x < - inimigo.largura
+      
+        inimigo.exibe();
+        inimigo.move();
+      
+        if (inimigoVisivel) {
+          this.inimigoAtual++;
+          if (this.inimigoAtual >= inimigos.length) {
+            this.inimigoAtual = 0;
+          }
+          inimigo.velocidade = parseInt(random(10, 30));
+        }
+      
+        if (personagem.estaColidindo(inimigo)) {
+          image(imagemGameOver, 800 / 2 - 200, 600 / 3);
+          noLoop();
+          trilhaSonora.stop();
+        }
+    }
+}
---
+++ scripts/load.js
@@ -0,0 +1,12 @@
+function preload() {
+    imagemPlanoDeFundo = loadImage('imagens/cenario/floresta.png');
+    imagemGameOver = loadImage('imagens/assets/game-over.png');
+    imagemHipsta = loadImage('imagens/personagem/correndo.png');
+    imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+    imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
+    imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
+    
+    trilhaSonora = loadSound('sons/trilha_jogo.mp3');
+    somDoPulo = loadSound('sons/somPulo.mp3');
+  }
+  
